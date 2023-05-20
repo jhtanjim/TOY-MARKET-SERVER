e60d55567f2208ccb7667221f7ce4670c62fc85f
jhtanjim~ <jh.tanjim.4@gmail.com>
2023-05-20 20:34:10 +0600

    category

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-console.log(process.env.DB_USER);
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.khwex9e.mongodb.net/?retryWrites=true&w=majority`;
@@ -30,6 +29,41 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+
+        const toyCollection = client.db('toyDB').collection('toy')
+        const categoryCollection = client.db('toyDB').collection('toyCategory')
+
+
+
+        // receive data from client by newToy
+        // create
+        app.post('/toy', async (req, res) => {
+
+            const newToy = req.body
+            console.log(newToy);
+            const result = await toyCollection.insertOne(newToy)
+            res.send(result)
+        })
+        // Read
+        app.get('/toy', async (req, res) => {
+            const cursor = toyCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+        app.get('/toyCategory', async (req, res) => {
+            const cursor = categoryCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+
+
+
+
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
