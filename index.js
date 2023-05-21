80284fefa2eeedd66e6bcc437f63db3639391cc1
jhtanjim~ <jh.tanjim.4@gmail.com>
2023-05-21 22:59:18 +0600

    done

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
 
         const toyCollection = client.db('toyDB').collection('toy')
@@ -159,9 +159,9 @@ run().catch(console.dir);
 
 
 
-// app.get('/', (req, res) => {
-//     res.send('Toy store server running')
-// })
+app.get('/', (req, res) => {
+    res.send('Toy store server running')
+})
 
 
 app.listen(port, () => {
